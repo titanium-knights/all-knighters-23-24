2d9c4a3fa8789d90180536a67322899c19d0b44b
alisona <alile24@gmail.com>
2023-10-06 17:03:59 -0400

    skel

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp_Practice.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//Welcome to our TeleOp code
+@TeleOp
+public class TeleOp_Practice extends OpMode {
+    //declare objects of subsystems
+
+    @Override //runs on startup of the app
+    public void init() {
+
+    }
+
+    @Override //runs continuously
+    public void loop() {
+
+    }
+}
