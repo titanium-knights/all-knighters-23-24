413a645a4d83ea074d00fc632bdc3589b4c0c99d
alisona <alile24@gmail.com>
2023-10-06 17:03:46 -0400

    Configs!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Config.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.util;
+
+public class Config {
+    //We use the CONFIG to store all of our port numbers. This allows us to have them all in one place.
+
+    //Drivetrain
+
+
+    //Subsystems
+}
