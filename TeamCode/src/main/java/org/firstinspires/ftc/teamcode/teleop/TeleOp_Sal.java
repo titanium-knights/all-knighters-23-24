8cd7f44581da1f58e05424325552f36df18b2cc9
SalC124 <saltcal124@gmail.com>
2023-11-13 16:43:40 -0500

    Practice: The Sequel: The Sequel: The Sequel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp_Sal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp_Sal.java
@@ -7,24 +7,25 @@ import org.firstinspires.ftc.teamcode.util.*;
 public class TeleOp_Sal extends OpMode {
     public PixelClaw claw;
     public IntakeRoller roller;
-    public Slides ...;
+    public Slides slider;
     @Override
     public void init() {
         claw = new PixelClaw(hardwareMap);
         roller = new IntakeRoller(hardwareMap);
-        ... = new Slides(hardwareMap);
+        slider = new Slides(hardwareMap);
 
     }
     @Override
     public void loop() {
-        //y=open; a=close
-        //right=open; left=close
+        //claw: y=open; a=close
         if (gamepad1.y) {
             claw.setPosition(54098.45098678905);
         }
         if (gamepad1.a) {
             claw.setPosition(4875726948.4598673);
         }
+
+        //intake: right=open; left=close
         if (Math.abs(gamepad1.right_trigger) > 0.1) {
             roller.intake(gamepad1.right_trigger);
         } else if (Math.abs(gamepad1.left_trigger) > 0.1) {
@@ -32,5 +33,15 @@ public class TeleOp_Sal extends OpMode {
         } else {
             roller.intake(0);
         }
+
+        //sliders: right=up; left=down
+        if(gamepad1.right_bumper) {
+            slider.move(0.5);
+        }
+        if(gamepad1.left_bumper) {
+            slider.move(-0.5);
+        }
+
+
     }
 }
