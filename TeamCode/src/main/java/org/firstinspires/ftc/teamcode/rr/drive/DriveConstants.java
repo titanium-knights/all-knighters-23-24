2575241d7061e11a58f3cb30b18ec7e67b5293bb
aliisona <alile24@bergen.org>
2023-12-31 23:44:37 -0500

    newest updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr/drive/DriveConstants.java
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0154;
-    public static double kA = 0.003;
-    public static double kStatic = 0;
+    public static double kV = 0.014;
+    public static double kA = 0.002875;
+    public static double kStatic = 0.01;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -67,7 +67,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 73.17330064499293;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
+    public static double MAX_ANG_VEL = Math.toRadians(274.28385676746996);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
     /*
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr/drive/StandardTrackingWheelLocalizer.java
@@ -32,10 +32,10 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double TICKS_PER_REV = 8192;
     public static double WHEEL_RADIUS = 0.6889764; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
-    public static double LATERAL_DISTANCE = 11.375; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 11.205; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = -.5; // in; offset of the lateral wheel
-    public static double X_MULTIPLIER = 1;
-    public static double Y_MULTIPLIER = 1;
+    public static double X_MULTIPLIER = 1.018;
+    public static double Y_MULTIPLIER = 1.013;
     private Encoder leftEncoder, rightEncoder, frontEncoder;
 
     private List<Integer> lastEncPositions, lastEncVels;
